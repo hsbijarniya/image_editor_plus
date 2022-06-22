6cdfa47f2002b973f098e76c03d0cb15bf824166
DizzasTeR <dizzastermta@gmail.com>
2022-06-22 17:09:58 +0500

    Revert main to original

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:image_editor_plus/image_editor_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,27 +49,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  Uint8List? imageData;
 
-  void _incrementCounter() async {
-    var editedImage = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ImageEditor(
-          allowGallery: true,
-        ),
-      ),
-    );
-
-    // replace with edited image
-    if (editedImage != null) {
-      setState(() {
-		imageData = editedImage;
-	  });
-    }
-    // setState(() {
-    //   _counter++;
-    // });
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
@@ -93,6 +79,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -102,7 +102,6 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-			imageData != null ? Image.memory(imageData!) : const SizedBox.shrink()
           ],
         ),
       ),
