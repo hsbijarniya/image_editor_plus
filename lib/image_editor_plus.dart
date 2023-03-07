9d058d6d274d8760ae87689280dd3de9b842d00b
danyaaksenov-spec <entering747@gmail.com>
2023-03-08 01:23:48 +0200

    - try5

--- lib/image_editor_plus.dart
+++ lib/image_editor_plus.dart
@@ -387,32 +387,37 @@ class _SingleImageEditorState extends State<SingleImageEditor> {
         icon: const Icon(Icons.check),
         onPressed: () async {
           resetTransformation();
-          // _onLoading();
-          var i = await screenshotController.capture(pixelRatio: pixelRatio);
-          Navigator.pop(context, i);
+          _saving();
         },
       ),
     ];
   }
 
-  void _onLoading() {
-    showDialog(
+  Future<void> _saving() async {
+    await showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return Dialog(
-          backgroundColor: Colors.grey.shade100,
+          backgroundColor: Colors.transparent,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: const [
-              CircularProgressIndicator(),
+              CircularProgressIndicator(
+                color: Colors.black26,
+              ),
               Text("Loading..."),
             ],
           ),
         );
       },
     );
+    Future.delayed(const Duration(milliseconds: 500), () async {
+      await screenshotController.capture(pixelRatio: pixelRatio).then((value) {
+        Navigator.pop(context, value);
+      });
+    });
   }
 
   @override
